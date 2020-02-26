37f1e4152684df3c68f8a59b6ec7e4d7b96ee5b2
Rohit Karyappa <rohit.karyappa@wuerth-phoenix.com>
2020-02-26 08:16:01 +0530

    ADD HomeFeedViewController
    Added HomeFeedViewController class which will be the first screen/home screen for the app

---
+++ Je2/Je2/HomeFeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeFeedViewController.swift
+//  Je2
+//
+//  Created by Rohit Karyappa on 2/26/20.
+//  Copyright © 2020 Rohit Karyappa. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
