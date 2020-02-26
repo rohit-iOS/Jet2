2e61e92a59840886db73c0a54b8cb8c1ad4e8cf3
Rohit Karyappa <rohit.karyappa@wuerth-phoenix.com>
2020-02-26 07:15:39 +0530

    ADD Project initial setup.
    Created single view  app project to start off
    
    (JIRA ticket number)

---
+++ Je2/Je2Tests/Je2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Je2Tests.swift
+//  Je2Tests
+//
+//  Created by Rohit Karyappa on 2/26/20.
+//  Copyright © 2020 Rohit Karyappa. All rights reserved.
+//
+
+import XCTest
+@testable import Je2
+
+class Je2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
